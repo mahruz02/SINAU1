eed6ce000385f5a0ad5642e9d69e11d4ea8470d8
mahruz02 <bamsseventeen@gmail.com>
2021-07-09 11:30:20 +0700

    bismillah

---
+++ lib/models/Product.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+class Product {
+  final int id;
+  final String title, description, price;
+  final List<String> images;
+  final List<Color> colors;
+  final double rating;
+  final bool isFavourite, isPopular;
+
+  Product({
+    @required this.id,
+    @required this.images,
+    @required this.colors,
+    this.rating = 0.0,
+    this.isFavourite = false,
+    this.isPopular = false,
+    @required this.title,
+    @required this.price,
+    @required this.description,
+  });
+}
+
+// Our demo Products
+
+List<Product> demoProducts = [
+  Product(
+    id: 1,
+    images: [
+      "assets/images/guru1.png",
+      "assets/images/guru2.png",
+    ],
+    colors: [
+      Color(0xFFF6625E),
+      Color(0xFF836DB8),
+      Color(0xFFDECB9C),
+      Colors.white,
+    ],
+    title: "Sri Rahayu S.Pd",
+    price: "300.000",
+    description: description,
+    rating: 4.8,
+    isFavourite: true,
+    isPopular: true,
+  ),
+  Product(
+    id: 2,
+    images: [
+      "assets/images/Image Popular Product 2.png",
+    ],
+    colors: [
+      Color(0xFFF6625E),
+      Color(0xFF836DB8),
+      Color(0xFFDECB9C),
+      Colors.white,
+    ],
+    title: "Muhamad Cahyono Ihwan",
+    price: "250.000",
+    description: description,
+    rating: 4.1,
+    isPopular: true,
+  ),
+  Product(
+    id: 3,
+    images: [
+      "assets/images/guru 2.png",
+    ],
+    colors: [
+      Color(0xFFF6625E),
+      Color(0xFF836DB8),
+      Color(0xFFDECB9C),
+      Colors.white,
+    ],
+    title: "Dwi Satriyo",
+    price: "150.000",
+    description: description,
+    rating: 4.1,
+    isFavourite: true,
+    isPopular: true,
+  ),
+  Product(
+    id: 4,
+    images: [
+      "assets/images/wireless headset.png",
+    ],
+    colors: [
+      Color(0xFFF6625E),
+      Color(0xFF836DB8),
+      Color(0xFFDECB9C),
+      Colors.white,
+    ],
+    title: "Ananda Fransiska S.Sos",
+    price: "210.000",
+    description: description,
+    rating: 4.1,
+    isFavourite: true,
+    isPopular: true,
+  ),
+];
+
+const String description =
+    "I am a UGM graduate and have a certificate to teach your child the knowledge they need...";
